c3c97eede3e399969ae8eff71677f9c14afc9545
Marc Kerr <mkerr@iu.edu>
2020-01-24 11:58:59 -0500

    Script posted

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RemoveFlash
-Script for removing Adobe Flash Player form a Mac
+
+This script removes Adobe FlashPlayer as well as preferences that may
+be in the users home directory. Use this script as you see fit and
+improve it. This script may not work as you expect so please test.
+There is no warranty or guarantee. Buy using this script you accepte
+the consequences of its actions. If you find a problem with this
+script say so in the issues.
---
+++ RemoveFlash.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This script removes Adobe FlashPlayer as well as preferences that may
+# be in the users home directory. Use this script as you see fit and
+# improve it. This script may not work as you expect so please test.
+# There is no warranty or guarantee. Buy using this script you accepte
+# the consequences of its actions. If you find a problem with this
+# script say so in the issues.
+
+# last edit 2020_01_24
+
+removeThis=("/Library/Application Support/Macromedia" \
+			"/Library/Application Support/Adobe/Flash Player Install Manager" \
+			"/Library/Internet Plug-Ins/Flash Player.plugin" \
+			"/Library/Internet Plug-Ins/flashplayer.xpt" \
+			"/Library/Internet Plug-Ins/PepperFlashPlayer" \
+			"/Library/LaunchDaemons/com.adobe.fpsaud.plist" \
+			"/Library/PreferencePanes/Flash Player.prefPane" \
+			"/Applications/Utilities/Adobe Flash Player Install Manager.app")
+
+for removeThis in "${removeThis[@]}" ; do
+	if [ -e "$removeThis" ] ; then
+		echo "Removing $removeThis"
+		rm -rf "$removeThis"
+	fi
+done
+
+# iterate through list of folders in /Users
+for folder in /Users/*; do
+    user=$(basename "${folder}")
+    # compare folder name against the array items
+    if [ -e "/Users/${user}/Library/Preferences/Macromedia/Flash Player" ]; then
+    	echo "Removing /Users/${user}/Library/Preferences/Macromedia/Flash Player"
+        rm -rf "/Users/${user}/Library/Preferences/Macromedia/Flash Player"
+    fi
+    if [ -e "/Users/${user}/Library/Caches/Adobe/Flash Player" ]; then
+    	echo "Removing /Users/${user}/Library/Caches/Adobe/Flash Player"
+        rm -rf "/Users/${user}/Library/Caches/Adobe/Flash Player"
+    fi
+    
+done
